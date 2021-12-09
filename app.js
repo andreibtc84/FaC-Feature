5115c5c005ded614a1d3928d2c3ee7ba342b6c24
Andrei Btc <andreibtc@gmail.com>
2021-12-09 16:33:25 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Feature - Changing the background gradient as per user input
+const gradientStart = document.querySelector("#gradient-start");
+const gradientEnd = document.querySelector("#gradient-end");
+const btn = document.querySelector("#btn");
+
+// Event listeners
+
+gradientStart.addEventListener("input", () => {
+  return gradientStart.value;
+});
+
+gradientEnd.addEventListener("input", () => {
+  return gradientEnd.value;
+});
+
+btn.addEventListener("click", () => {
+  document.body.style.background = `linear-gradient(to bottom, ${gradientStart.value}, ${gradientEnd.value})`;
+});
